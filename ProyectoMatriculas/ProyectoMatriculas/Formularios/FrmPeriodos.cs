271906c8d23685883e9e2317b7f4e9e57f847996
CristianDom <cristian.dominguez3@gmail.com>
2015-07-19 00:41:29 -0500

    cambios

--- ProyectoMatriculas/ProyectoMatriculas/Formularios/FrmPeriodos.cs
+++ ProyectoMatriculas/ProyectoMatriculas/Formularios/FrmPeriodos.cs
@@ -15,5 +15,10 @@ namespace ProyectoMatriculas.Formularios
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
