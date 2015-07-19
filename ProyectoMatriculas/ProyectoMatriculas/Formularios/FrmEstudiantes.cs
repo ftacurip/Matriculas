dde07f4df025eff7dc8c257a82d1672f40f463d0
Fredy Tacuri <ftacurip@hotmail.com>
2015-07-19 01:56:34 -0500

    Update Alumnos

--- ProyectoMatriculas/ProyectoMatriculas/Formularios/FrmEstudiantes.cs
+++ ProyectoMatriculas/ProyectoMatriculas/Formularios/FrmEstudiantes.cs
@@ -39,6 +39,28 @@ namespace ProyectoMatriculas.Formularios
             DTFechaNacimiento.Value = System.DateTime.Today;
         }
 
+        void CargarAlumnos()
+        {
+            #region :: Variables Utilizadas ::
+
+            Clases.Alumno AlumnosConsulta = new Clases.Alumno();
+
+            #endregion
+
+            #region :: Variables Utilizadas ::
+
+            DataTable DtAlumnos = new DataTable();
+
+            #endregion
+
+            DtAlumnos = AlumnosConsulta.GetTodosLosAlumnos();
+
+            if (DtAlumnos.Rows.Count > 0)
+            {
+                DgvEstudiante.DataSource = DtAlumnos;
+            }
+        }
+
         #endregion
 
         public FrmEstudiantes()
@@ -74,11 +96,34 @@ namespace ProyectoMatriculas.Formularios
                 NuevoAlumno.Direccion = TxtDireccion.Text.ToUpper();
                 NuevoAlumno.Telefono = TxtTelefono.Text;
                 NuevoAlumno.Fecha_Nacimiento = DTFechaNacimiento.Value.ToString("yyyy-MM-dd HH:mm:ss");
-                if (NuevoAlumno.IngresarNuevoAlumno(NuevoAlumno) == true) { MessageBox.Show("El estudiante ha sido registrado exitosamente."); LimpiarControles(); }
+                if (NuevoAlumno.IngresarNuevoAlumno(NuevoAlumno) == true) { MessageBox.Show("El estudiante ha sido registrado exitosamente."); LimpiarControles(); CargarAlumnos(); }
                 else { MessageBox.Show("El estudiante NO ha podido ser registrado."); }
             }
             else { }
 
+        }
+
+        private void FrmEstudiantes_Load(object sender, EventArgs e)
+        {
+            CargarAlumnos();
+        }
+
+        private void DgvEstudiante_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(DgvEstudiante.Rows.Count> 0)
+            {
+                TxtCedula.Text = DgvEstudiante.Rows[e.RowIndex].Cells["Cedula"].Value.ToString(); TxtCedula.ReadOnly = true;
+                TxtNombre.Text = DgvEstudiante.Rows[e.RowIndex].Cells["Nombre"].Value.ToString();
+                TxtApellido.Text = DgvEstudiante.Rows[e.RowIndex].Cells["Apellido"].Value.ToString();
+                TxtDireccion.Text= DgvEstudiante.Rows[e.RowIndex].Cells["Direccion"].Value.ToString();
+                TxtTelefono.Text = DgvEstudiante.Rows[e.RowIndex].Cells["Telefono"].Value.ToString();
+                DTFechaNacimiento.Value = Convert.ToDateTime(DgvEstudiante.Rows[e.RowIndex].Cells["Fecha_Nacimiento"].Value.ToString());
+            }
+        }
+
+        private void BtnNuevo_Click(object sender, EventArgs e)
+        {
+            TxtCedula.ReadOnly = true;
         }
     }
 }
